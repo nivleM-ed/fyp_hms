22ba6323281d743104b8cc30eb3a0c8f20f49807
Melvin Edward <melvinedward98@gmail.com>
2020-12-19 14:45:24 +0800

    Improved NotFoundPage

--- screen/src/pages/NotFoundPage.vue
+++ screen/src/pages/NotFoundPage.vue
@@ -1,8 +1,51 @@
 <template>
   <v-container>
-      <p>NOT FOUND PAGE</p>
+    <v-card flat class="text-center" min-height="500px">
+      <v-img src="img/thinking_gif.gif" contain :height="height"></v-img>
+    </v-card>
+    <div class="text-center">
+      <p class="m-4">CANNOT FIND THE PAGE YOU WANTED</p>
+      <v-btn color="green" @click="$router.push('/main/')"
+        >Return to Homepage</v-btn
+      >
+    </div>
   </v-container>
 </template>
 
-<style>
-</style>
+<style></style>
+<script>
+export default {
+  data() {
+    return {
+      height: 100,
+      token: true,
+      troller: null
+    };
+  },
+  async created() {
+    this.troll();
+  },
+  methods: {
+    troll() {
+      this.troller = setInterval(() => {
+        if (this.token) {
+          if (this.height == 400) {
+            this.token = false;
+          } else {
+            this.height += 1;
+          }
+        } else {
+          if (this.height == 100) {
+            this.token = true;
+          } else {
+            this.height -= 1;
+          }
+        }
+      }, 10);
+    },
+  },
+  destroyed() {
+    clearInterval(this.troller)
+  }
+};
+</script>
